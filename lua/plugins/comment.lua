8b42df19de2387fa0dc51f9960543123e8e8e839
shuishouy <yjtpost@gmail.com>
2025-11-01 19:43:13 +0800

    improve multi-lines comment action

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,10 +1,15 @@
 return {
   'numToStr/Comment.nvim',
   vim.keymap.set("n", "<C-/>", function()
-    require("Comment.api").toggle.linewise.current()
+    require('Comment.api').toggle.linewise.current()
   end),
-  vim.keymap.set("v", "<C-/>", function()
-    require("Comment.api").toggle.linewise()
-  end),
+
+  vim.keymap.set('x', '<C-/>', function()
+    local api = require 'Comment.api'
+    local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    api.locked 'toggle.linewise'(vim.fn.visualmode())
+    vim.cmd 'normal! gv'
+  end, { desc = 'Comment toggle linewise (visual) and preserve the visual selection' }),
   opts = { }
 }
